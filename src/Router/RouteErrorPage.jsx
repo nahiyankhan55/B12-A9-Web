d7ae8d25d94543607ec769af4b49dc7102955183
nahiyankhan55 <kushbuladesignmax+++@gmail.com>
2025-12-03 13:31:09 +0600

    error page add

--- src/Router/RouteErrorPage.jsx
+++ src/Router/RouteErrorPage.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router";
+import errorImage from "/error404.gif";
+
 const RouteErrorPage = () => {
-  return <div>RouteErrorPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-1">
+      <img className="max-w-lg w-full" src={errorImage} alt="error-gif" />
+      <p className="text-xl font-semibold text-red-500">Page Not Found!</p>
+      <Link
+        className="py-1 px-6 rounded-lg bg-green-600 hover:bg-green-800 duration-300 transition font-medium text-white text-lg mt-2"
+        to={"/"}
+      >
+        Go back!
+      </Link>
+    </div>
+  );
 };
 
 export default RouteErrorPage;
--- src/components/layouts/Plants/Plants.jsx
+++ src/components/layouts/Plants/Plants.jsx
@@ -33,13 +33,13 @@ const Plants = () => {
         for every plant lover.
       </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
         {data.map((plant) => (
           <div
             key={plant.plantId}
             data-aos="zoom-in"
             className="bg-white rounded-2xl shadow-md p-5 hover:shadow-lg transition-all duration-300 border border-green-100"
-          > 
+          >
             <img
               src={plant.image}
               alt={plant.plantName}
@@ -48,14 +48,17 @@ const Plants = () => {
             <h3 className="text-xl font-semibold text-green-800">
               {plant.plantName}
             </h3>
-            <p className="text-gray-500 mb-2">{plant.category}</p>
-            <div className="flex justify-between items-center text-sm mb-3">
+            <p className="text-gray-500 mb-2 font-medium">{plant.category}</p>
+            <p className="text-gray-700 mt-1 text-sm line-clamp-2">
+              {plant.description}
+            </p>
+            {/* <div className="flex justify-between items-center text-sm mb-3 mt-3">
               <span className="font-medium text-green-700">${plant.price}</span>
               <span className="text-yellow-500 flex items-center gap-1">
                 <FaStar className="text-lg"></FaStar> {plant.rating}
               </span>
-            </div>
-            <div>
+            </div> */}
+            <div className="mt-3">
               <Link to={`/plant/${plant.plantId}`}>
                 <p className="w-full bg-green-600 text-white py-2 rounded-md font-medium hover:bg-green-700 transition text-center">
                   View Details
